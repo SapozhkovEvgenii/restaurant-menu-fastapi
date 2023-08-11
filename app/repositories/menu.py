09b8b51eaeb50e314880c693e9c3ea4337a02a6c
Evgeny Sapozhkov <sapozhkovevgenii@gmail.com>
2023-08-11 21:10:22 +0300

    Added 'get_all_data' method to the MenuRepository

--- app/repositories/menu.py
+++ app/repositories/menu.py
@@ -1,10 +1,27 @@
-from app.models import Menu
+from typing import Any
 
-from .base import BaseRepository
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
+
+from app.models import Menu, SubMenu
+from app.repositories.base import BaseRepository
 
 
 class MenuRepository(BaseRepository):
-    pass
+
+    async def get_all_data(self, session: AsyncSession) -> Any:
+        """Receive all menu data with all submenus with their dishes."""
+
+        result = await session.scalars(
+            select(self.model).options(
+                joinedload(self.model.submenus).joinedload(SubMenu.dishes)
+            )
+        )
+        menus = result.unique().all()
+        menu_data = jsonable_encoder(menus)
+        return menu_data
 
 
 menu_crud_repository = MenuRepository(Menu)  # type: ignore
